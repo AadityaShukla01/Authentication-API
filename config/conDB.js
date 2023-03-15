b8abe16c9872d3a34eaf515c7eb36b8d84b45009
Aaditya Shukla <95955543+AadityaShukla01@users.noreply.github.com>
2023-03-15 15:49:05 +0530

    Update conDB.js

--- config/conDB.js
+++ config/conDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const conDB = async (DATABASE_URL) => {
     try {
         const DB_OPTION = {
-            dbname: "sai"
+            dbname: "db"
         }
         await mongoose.connect(DATABASE_URL, DB_OPTION)
         console.log('Con')
